87cf491a3b78684329279a846076325ec046603a
bellowswind <bellowswind@mail.ru>
2017-09-05 00:44:18 +1000

    Change a little for bat file

--- edit.bat
+++ edit.bat
@@ -1,1 +1,16 @@
+
+git rebase --committer-date-is-author-date
+
+
+
+-----------How to get all history including auther and committer user and date
+https://stackoverflow.com/questions/14243380/how-to-configure-git-log-to-show-commit-date
+
+git log --pretty-fuller
+
+-----------How to change auther date
 git commit --amend --date="2010-03-11T11:20:10±00:01" -C f666e41e596e40b6dacb30ac5004f3ed4dff6a7c
+
+
+-----------How to change committer date
+https://stackoverflow.com/questions/28536980/git-change-commit-date-to-author-date
